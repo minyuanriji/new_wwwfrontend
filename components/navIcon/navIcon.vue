9dbdafb7a8d87be04213ab2163e3b3348065e380
zx-1992 <1447966060@qq.com>
2021-12-20 11:19:46 +0800

    12.20

---
+++ components/navIcon/navIcon.vue
@@ -0,0 +1,150 @@
+<template>
+	<view class="navIconBox" :style="{'background':background,'color':textColor}">
+		
+		<view class="flex list" v-if="listNums == 3">
+			<view v-for="(item,index) in list" :key='index' class="tui-col-4 listBox">
+				<view class="listBox_nav flex flex-col flex-y-center flex-x-center" @tap="navTo(item.url,item.name,item.params)">
+					<image class="listImg" :src="item.icon" mode=""></image>
+					<view class="navName" :style="{color: textColor}">{{item.name}}</view>
+				</view>
+			</view>
+		</view>
+		<view class="flex list" v-if="listNums == 4">
+			<view v-for="(item,index) in list" :key='index' class="tui-col-3 listBox">
+				<view class="listBox_nav flex flex-col flex-y-center flex-x-center" @tap="navTo(item.url,item.name,item.params)">
+					<image class="listImg" :src="item.icon" mode=""></image>
+					<view class="navName" :style="{color: textColor}">{{item.name}}</view>
+				</view>
+			</view>
+		</view>
+		<view class="flex list" v-if="listNums == 5">
+			<view v-for="(item,index) in list" :key='index' class="listBox listBox5">
+				<view class="listBox_nav flex flex-col flex-y-center flex-x-center" @tap="navTo(item.url,item.name,item.params)">
+					<image class="listImg" :src="item.icon" mode=""></image>
+					<view class="navName" :style="{color: textColor}">{{item.name}}</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "navIcon",
+		props: {
+			list: {
+				type: Array,
+				default: ()=>[]
+			},
+			background: {
+				type: String,
+				default: '#ffffff'
+			},
+			textColor: {
+				type: String,
+				default: '#000000'
+			},
+			listNums: {
+				type: Number,
+				default: 1
+			},
+			rowNums: {
+				type: Number,
+				default: 1
+			},
+			url: {
+				type: String,
+				default: ''
+			},
+		},
+		methods:{
+			navTo(url,title,params){
+				if(title == '直播专区'){
+					console.log(url);
+					uni.navigateTo({
+						url:'/pages/live/live'
+					})
+					return false;
+				}
+				if (url) {
+					if (url.split('=')[0] == '/pages/web/web?url') {
+						//#ifdef H5
+						window.location.href = decodeURIComponent(url.split('=')[1]);
+						//#endif
+						
+						//#ifdef MP-WEIXIN
+							uni.navigateTo({
+								url:'/pages/webview/webview?url=' + url.split('=')[1]
+							})
+						//#endif
+						
+					}else if(url.split('=')[0] == 'tel?tel'){
+						uni.makePhoneCall({
+						    phoneNumber: url.split('=')[1], 
+							success: (res)=>{
+								this.$http.toast('打电话回调成功！')
+							}
+						});
+					}else if(url.split('=')[0] == 'app?app_id'){
+						let appId=params[0].value
+						let path=params[1].value
+						//#ifdef MP-WEIXIN
+							 wx.navigateToMiniProgram({
+								appId: appId,
+								path: path,
+								// extraData: {  //参数
+								//     mall: '1'
+								// },
+								envVersion: 'release',// 打开正式版
+								success(res) {
+									   // 打开成功
+								},
+								fail: function (err) {
+									console.log(err);
+							    }
+							})
+						//#endif
+						//#ifdef H5
+							this.$http.toast('请前往小程序访问');
+						//#endif
+					}else{
+						uni.navigateTo({
+							url
+						})
+					}
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import  '../../plugins/style/thorui.css';
+	.list{
+		flex-wrap: wrap;
+		overflow: hidden;
+		display: flex;
+		padding: 10rpx 0;
+	}
+	.listBox{
+		height: 140rpx;
+		text-align: center;
+		// flex: 1;
+	}
+	.listBox_nav{
+		width: 100%;
+		height: 100%;
+	}
+	.listImg {
+		width: 84rpx;
+		height: 84rpx;
+		margin-bottom: 6rpx;
+	}
+	.navName{
+		font-size: 24rpx;
+	}
+	.listBox5{
+		width: 150rpx;
+		// width: 142rpx;
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="home_container">
-		<view class="header">
+		<view class="home_header">
 			<view class="search_box">
 				<view class="checksao">
 					<view style="width: 80%;color: #000;font-size: 30rpx;font-weight: bold;overflow: hidden;
@@ -14,74 +14,143 @@
 					<view class="citiLOGO_select">
 						<image src="https://dev.mingyuanriji.cn/web/plugins//home_up.png" mode=""></image>
 					</view>
-				</view>	
+				</view>
 				<view class="search">
-					<search message="大家都在搜索" textAlign="56" frameColor="#FFFFFF"
-					 innerFrameColor="#F2F2F2" textColor="#555555" :borderRadius="56"></search>
+					<search message="大家都在搜索" textAlign="56" frameColor="#FFFFFF" innerFrameColor="#F2F2F2"
+						textColor="#555555" :borderRadius="56"></search>
 				</view>
 			</view>
+		</view>
+		<view class="home_bannersBox">
+			<banners :bannerData="bannersData"></banners>
+		</view>
+		<view class="home_navBar">
+			<navIcon class="navIcon_style" :list="navs" textColor="#353535" :rowNums="4" background="#ffffff"
+				:listNums="3"></navIcon>
+		</view>
+		<view class="home_good_list">
+			<vouchers :list='goods_ist'></vouchers>
 		</view>
 	</view>
 </template>
 
 <script>
 	import search from '@/components/search/search.vue';
-	export default{
-		components:{
-			search
+	import banners from '@/components/banners/banners';
+	import navIcon from '@/components/navIcon/navIcon';
+	import vouchers from '@/components/vouchers/vouchers.vue';
+	export default {
+		components: {
+			search,
+			banners,
+			navIcon,
+			vouchers
 		},
-		data(){
-			return{
-				
+		data() {
+			return {
+				bannersData: { //轮播图
+					fill: 1,
+					height: 390,
+					style: 1,
+					banners: [{
+							openType: "navigate",
+							params: [],
+							picUrl: "https://www.mingyuanriji.cn/web//uploads/images/original/20211116/588858be835b03a9195657c7f004b5fe.jpg",
+							url: "/oil/oilHome/oilHome"
+						},
+						{
+							openType: "navigate",
+							params: [],
+							picUrl: "https://www.mingyuanriji.cn/web//uploads/images/original/20211014/bd6f29fb30b9388644e93af273c1917b.jpg",
+							url: "/pages/diy/diy?page_id=133"
+						}
+					]
+				},
+				navs: [ //金刚区
+					{
+						icon: "https://www.mingyuanriji.cn/web//uploads/images/original/20211028/12582f156f4f10c272538c2c75698480.jpg",
+						name: "热卖爆品",
+						openType: "navigate",
+						url: "/aggregation/sellingHot/sellingHot",
+					},
+				],
+				goods_ist: [{
+					cover_pic: "http://yingmlife-1302693724.cos.ap-guangzhou.myqcloud.com/uploads/images/original/20211018/af8287e1940ea564d15c60345c790bea.jpg",
+					enable_express: "0",
+					freight_id: "5",
+					goods_stock: "79751",
+					got_shopping_voucher_num: 0.1,
+					id: "3483",
+					is_level: "1",
+					is_show_sales: "1",
+					max_deduct_integral: "0.00",
+					mch_id: "0",
+					name: "牙刷成人软毛10-20支套装",
+					original_price: "39.00",
+					price: "0.10",
+					sales: "已售1315件",
+					status: "1",
+					unit: "件",
+					use_attr: "1",
+					use_virtual_sales: "1",
+					video_url: "",
+					virtual_sales: "1256",
+				}]
 			}
 		}
 	}
 </script>
 
 <style lang="less">
-	.home_container{
+	.home_container {
 		width: 100%;
 		overflow: hidden;
+
 		.search_box {
 			position: fixed;
 			/* #ifdef H5||MP*/
-				top: 80rpx;
-				height: 100rpx;
+			top: 80rpx;
+			height: 100rpx;
 			/* #endif */
 			width: 100%;
 			/* #ifdef APP-PLUS */
-				top: 40rpx;
-				height: 100rpx;
+			top: 40rpx;
+			height: 100rpx;
 			/* #endif */
 			z-index: 99;
 			display: flex;
 			background: #fff;
-			.checksao{
+
+			.checksao {
 				width: 35%;
 				background: #fff;
 				position: relative;
-				.citiLOGO_location{
+
+				.citiLOGO_location {
 					width: 50rpx;
 					height: 96rpx;
 					position: absolute;
 					top: 0;
 					left: 20rpx;
-					z-index:9999;
-					image{
+					z-index: 9999;
+
+					image {
 						width: 40rpx;
 						height: 40rpx;
 						display: block;
 						margin-top: 33rpx;
 					}
 				}
-				.citiLOGO_select{
+
+				.citiLOGO_select {
 					width: 30rpx;
 					height: 96rpx;
 					position: absolute;
 					top: 10rpx;
-					right:0rpx;
-					z-index:9999;
-					image{
+					right: 0rpx;
+					z-index: 9999;
+
+					image {
 						width: 20rpx;
 						height: 20rpx;
 						display: block;
@@ -89,11 +158,15 @@
 					}
 				}
 			}
-			.search{
+
+			.search {
 				width: 75%;
 				margin-bottom: 30rpx;
 			}
 		}
-		
+
+		.bannersBox {
+			margin-top: 90rpx;
+		}
 	}
 </style>
